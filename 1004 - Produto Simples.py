7fac168aa2330d053fab81cb32ecc01a53830846
joaoGabArruda <107219050+joaoGabArruda@users.noreply.github.com>
2022-08-02 19:24:37 -0400

    Update 1004 - Produto Simples.py

--- 1004 - Produto Simples.py
+++ 1004 - Produto Simples.py
@@ -1,4 +1,6 @@
 # Linguagem:  Python 3.9 (Python 3.9.4) 
+# Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com
+# mensagem correspondente.
 
 A = int(input())
 B = int(input())
