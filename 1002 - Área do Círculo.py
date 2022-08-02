4ec9885185aedaf2457d775980efdffede9beb41
joaoGabArruda <107219050+joaoGabArruda@users.noreply.github.com>
2022-08-02 19:03:23 -0400

    Update and rename 1002 - Área do Círculo to 1002 - Área do Círculo.py

---
+++ 1002 - Área do Círculo.py
@@ -0,0 +1,6 @@
+# Linguagem: Python
+
+raio = float(input()) #recebe o input do teclado, converte em float e insere na vaiável raio
+n = 3.14159 # define n como sendo esse valor
+area = n * raio**2 # faz o cálculo da area
+print(f'A={area:.4f}') # faz o print ajustando o numero de casas decimais através de "f-strings"
