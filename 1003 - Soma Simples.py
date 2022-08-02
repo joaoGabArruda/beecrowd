5a504140d26cdb4fb70437dab5730f166cb8f858
joaoGabArruda <107219050+joaoGabArruda@users.noreply.github.com>
2022-08-02 19:23:50 -0400

    Update 1003 - Soma Simples.py

--- 1003 - Soma Simples.py
+++ 1003 - Soma Simples.py
@@ -1,4 +1,4 @@
-# Linguagem: Python
+# Linguagem: Python 3.9 (Python 3.9.4)
 # Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
 
 A = int(input())
