9928ce6a9517a12735f0f1a4fba704fca28e24c0
joaoGabArruda <107219050+joaoGabArruda@users.noreply.github.com>
2022-08-06 07:23:59 -0400

    Create  1008 - Salário.py

---
+++ 1008 - Salário.py
@@ -0,0 +1,10 @@
+# Linguagem: Python 3.9 (Python 3.9.4)
+
+numero = int(input())
+horas = int(input())
+valor = float(input())
+
+salario = horas * valor
+
+print(f'NUMBER = {numero}')
+print(f'SALARY = U$ {salario:.2f}')
