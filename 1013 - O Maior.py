eb812b35928877f437eec23373135706e9d70a69
joaoGabArruda <107219050+joaoGabArruda@users.noreply.github.com>
2022-08-09 16:38:13 -0400

    Update 1013 - O Maior.py

--- 1013 - O Maior.py
+++ 1013 - O Maior.py
@@ -1,6 +1,7 @@
 # Linguagem: Python 3.9 (Python 3.9.4)
 
-# 
+# Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+# Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
 
 lista = input()
 a,b,c = lista.split(" ")
