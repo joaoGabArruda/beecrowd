79d6c2c4ba0049777ddadec8896756ccfdea748a
Joao_Arruda <107219050+joaoGabArruda@users.noreply.github.com>
2022-08-10 19:45:00 -0400

    Create  1014 - Consumo.py

---
+++ 1014 - Consumo.py
@@ -0,0 +1,8 @@
+# Linguagem: Python 3.9 (Python 3.9.4) 
+
+# Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+X = int(input())
+Y = float(input())
+
+print(f'{X/Y:.3f} km/l')
