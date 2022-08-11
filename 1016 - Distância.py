e7389d16bd95ebb2483b7b84db6998d955edbff6
Joao_Arruda <107219050+joaoGabArruda@users.noreply.github.com>
2022-08-11 13:49:21 -0400

    Create  1016 - Distância.py

---
+++ 1016 - Distância.py
@@ -0,0 +1,11 @@
+# Linguagem: Python 3.9 (Python 3.9.4)
+
+# Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+# Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
+# Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
+
+
+km = int(input())
+
+print(f'{km*2} minutos') # multiplicar por 2 é o mesmo que dividir por 0,5. 
+# É feita a dividisão por 0,5 porque o carro faz 0,5 quilômetros por minuto
